e442d5efce7508f144426a28dbbe302c60c10ab1
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-05-24 23:32:44 +0530

    Add precompiled assets for Heroku

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const express = require("express");
 
 mongoose.Promise = global.Promise;
-//const uri="mongodb+srv://education_insight:Educationinsight@cluster0.5evzv.mongodb.net/education_insight?retryWrites=true&w=majority"
-const uri = "mongodb://127.0.0.1:27017/assignment-api-test";
+const uri="mongodb+srv://assignment:assignment@cluster0.e4lfz.mongodb.net/?retryWrites=true&w=majority"
+//const uri = "mongodb://127.0.0.1:27017/assignment-api-test";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
